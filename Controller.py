1eeade2003f94a4a6af10961f95d54f0f2c9e7e7
WojtekMs <wojtek.atmo@gmail.com>
2021-08-14 17:40:34 +0200

    First project definition

---
+++ Controller.py
@@ -0,0 +1,5 @@
+from board import
+
+class Controller:
+    def __init__(self, board):
+        self.board_ = board
---
+++ View.py
@@ -0,0 +1,5 @@
+from abc import ABC
+
+class View(ABC):
+    def display_board():
+        pass
